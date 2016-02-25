a059794f5be8b1878adaca9b21481332f2652ca8
Pongsakorn Sommalai <bongtrop@gmail.com>
2016-02-25 08:02:06 +0700

    Add log

--- app/src/main/java/xyz/pongsakorn/policeeye/activity/ScanningActivity.java
+++ app/src/main/java/xyz/pongsakorn/policeeye/activity/ScanningActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.animation.Animation;
 import android.view.animation.LinearInterpolator;
 import android.view.animation.TranslateAnimation;
