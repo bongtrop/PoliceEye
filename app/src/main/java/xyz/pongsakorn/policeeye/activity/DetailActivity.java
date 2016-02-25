33077e834c7f946e84ce056ce2d1f6461fd6d577
Pongsakorn Sommalai <bongtrop@gmail.com>
2016-02-25 08:35:19 +0700

    add dialog add remove scanningactivity

--- app/src/main/java/xyz/pongsakorn/policeeye/activity/DetailActivity.java
+++ app/src/main/java/xyz/pongsakorn/policeeye/activity/DetailActivity.java
@@ -1,17 +1,27 @@
 package xyz.pongsakorn.policeeye.activity;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
 
 import xyz.pongsakorn.policeeye.R;
+import xyz.pongsakorn.policeeye.utils.SketchMatchSDK;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -29,10 +39,18 @@ public class DetailActivity extends AppCompatActivity {
     EditText editNote;
     ImageView ivSketch;
 
+    SketchMatchSDK sketchMatchSDK;
+
+    String inputName;
+    String gender;
+    String note;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        sketchMatchSDK = new SketchMatchSDK("http://pongsakorn.xyz:8080");
 
         sketchBitmap = getIntent().getParcelableExtra("SketchImage");
 
@@ -47,7 +65,9 @@ public class DetailActivity extends AppCompatActivity {
         ivSketch = (ImageView) findViewById(R.id.ivSketch);
 
         ivSketch.setImageBitmap(sketchBitmap);
+
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -60,16 +80,48 @@ public class DetailActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         if (id == R.id.action_done) {
-            Intent intent = new Intent(DetailActivity.this, ScanningActivity.class);
-            intent.putExtra("SketchImage", sketchBitmap);
+            final Dialog dialog = new Dialog(this);
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            dialog.setCancelable(false);
+            dialog.setContentView(R.layout.dialog_loading);
+
+            dialog.show();
+
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            sketchBitmap.compress(Bitmap.CompressFormat.JPEG, 90, stream);
+
+            byte[] byteArray = stream.toByteArray();
+
+
+
             if (radGroupGender.getCheckedRadioButtonId() == R.id.radMale)
-                intent.putExtra("gender", "male");
+                gender = "male";
             else
-                intent.putExtra("gender", "female");
-            intent.putExtra("note", editNote.getText().toString());
-            intent.putExtra("inputName", editInputName.getText().toString());
-            startActivity(intent);
-            finish();
+                gender = "female";
+
+            note = editNote.getText().toString();
+            inputName = editInputName.getText().toString();
+
+            sketchMatchSDK.retrieval(byteArray, gender.equals("male") ? "M" : "F", new SketchMatchSDK.Listener() {
+                @Override
+                public void onSuccess(ArrayList<SketchMatchSDK.Person> people) {
+                    dialog.dismiss();
+                    Intent intent = new Intent(DetailActivity.this, ResultActivity.class);
+                    intent.putExtra("SketchImage", sketchBitmap);
+                    intent.putExtra("gender", gender);
+                    intent.putExtra("note", note);
+                    intent.putExtra("inputName", inputName);
+                    intent.putExtra("people", new Gson().toJson(people));
+                    startActivity(intent);
+                    finish();
+                }
+
+                @Override
+                public void onFail(String error) {
+                    Toast.makeText(getApplicationContext(), error, Toast.LENGTH_LONG).show();
+                    dialog.dismiss();
+                }
+            });
         }
 
         return super.onOptionsItemSelected(item);
